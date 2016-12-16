ab6081d9913d9d459bc16d30280f0ef11ebc29d4
mrmrs <hi@mrmrs.cc>
2016-12-16 00:40:05 +0000

    More explicit component naming

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var css = require('sheetify')
 var choo = require('choo')
 
 var ActualPopulation = require('./components/ActualPopulation')
-var WhitePerson = require('./components/WhitePerson')
+var WhiteMale = require('./components/WhiteMale')
 var Stats = require('./components/Stats')
 
 var data = require('./data')
@@ -28,13 +28,13 @@ function mainView () {
     <body class="App pa4">
       ${Stats({ wm: '1598944', population: '3893635' })}
 
-      ${WhitePerson({ data: data.collections[0] })}
+      ${WhiteMale({ data: data.collections[0] })}
       ${ActualPopulation({ data: data.collections[0], cells: data.collections[0].items.length, wm: '1598944', population: '3893635' })}
 
-      ${WhitePerson({ data: data.collections[1] })}
+      ${WhiteMale({ data: data.collections[1] })}
       ${ActualPopulation({ data: data.collections[1], cells: data.collections[1].items.length, wm: '1598944', population: '3893635' })}
 
-      ${WhitePerson({ data: data.collections[2] })}
+      ${WhiteMale({ data: data.collections[2] })}
       ${ActualPopulation({ data: data.collections[2], cells: data.collections[2].items.length, wm: '1598944', population: '3893635' })}
     </body>
   `
